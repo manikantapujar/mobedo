c4d1ce069c776c5d33db3e2a9b4a18c85547245e
manikantapujar <manikantapujar@gmail.com>
2023-01-13 23:28:30 +0530

    added about and home content

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,13 +16,13 @@ const AboutPage = () => {
       <Header />
       <Navbar />
       <Container className="maincontainer font" >
-        <div className="partion text-center">
+        <div className="partion text-center mt-2">
           <img className="yellowdot" src="../yellowdots.svg" alt="this is innovation"></img>
         </div>
-        <div className="about-heading text-center ">
+        <div className="about-heading text-center mt-2 mb-3">
           <h1>About Mobedo</h1>
         </div>
-        <Row className="row_content">
+        {/* <Row className="row_content">
           <Col>
             <div className="col_side_heading">
               <p>We have been delivering top-quality software solutions on time, on scope, on budget since 1992.</p>
@@ -31,31 +31,30 @@ const AboutPage = () => {
           <Col className="col_side_content">
             <img className="aboutus" src="../about-us.png" alt="this is Aboutus image"></img>
           </Col>
+        </Row> */}
+        <Row className="row_content_aboutus font">
+          <Col>
+            <img className="ourmission" src="../our-mission.png" alt="this is Aboutus image"></img>
+          </Col>
+          <Col className="col_side_content">
+            <div className="partion ">
+              <img className="yellowdot1" src="../yellowdots.svg" alt="this is innovation"></img>
+            </div>
+            <h2>Our Mission</h2>
+            <p className="aboutus_secondrow"> We continually strive to <b> DO MORE</b>, so our clients, our people and society
+              can <b> BE MORe</b></p>
+          </Col>
         </Row>
-      </Container>
-      <Row className="row_content_aboutus font">
-        <Col>
-          <img className="ourmission" src="../our-mission.png" alt="this is Aboutus image"></img>
-        </Col>
-        <Col className="col_side_content">
-          <div className="partion ">
-            <img className="yellowdot1" src="../yellowdots.svg" alt="this is innovation"></img>
-          </div>
-          <h2>Our Mission</h2>
-          <p className="aboutus_secondrow">At Sparsh, we work with passion & commitment to become a globally <br />
-            renowned software development company. With our ingenious software <br />
-            solutions, we help companies embrace digital transformation to improve  <br />
-            their business agility, optimize operations & boost their revenues.</p>
-        </Col>
-      </Row>
-      <Container className="maincontainer font" >
         <Row className="row_content ">
           <Col>
             <div className="partion ">
               <img className="yellowdot2" src="../yellowdots.svg" alt="this is innovation"></img>
             </div>
             <h2>Our Vission</h2>
-            <p className="aboutus_secondrow">We are committed to delivering value-added, high-quality IT solutions with a holistic approach that incorporates innovative thinking & collaborative techniques. By working alongside our clients we innovate on projects like never before and build their trust.</p>
+            <p className="aboutus_secondrow">To be recognized as a trusted and efficient partner to our clients and, craft
+              long standing relationships, while maintaining uncompromising principles as we
+              grow.
+              .</p>
           </Col>
           <Col className="col_side_content">
             <img className="ourvision" src="../our-vision.png" alt="this is Aboutus image"></img>
@@ -63,62 +62,76 @@ const AboutPage = () => {
         </Row>
         <Row className="row_content ">
           <Col>
-          <img className="ourvision" src="../our-vision.png" alt="this is Aboutus image"></img>
+            <img className="ourvision" src="../our-vision.png" alt="this is Aboutus image"></img>
           </Col>
           <Col className="col_side_content">
-          <div className="partion ">
+            <div className="partion ">
               <img className="yellowdot2" src="../yellowdots.svg" alt="this is innovation"></img>
             </div>
             <h2>Our Values</h2>
-            <p className="aboutus_secondrow">We are committed to delivering value-added, high-quality IT solutions with a holistic approach that incorporates innovative thinking & collaborative techniques. By working alongside our clients we innovate on projects like never before and build their trust.</p>
+            <ul className="values_li">
+              <li> <b> Karma:</b> We believe in the power of positive actions, and strive to make a positive impact through our work</li>
+              <li><b></b>Dharma: We believe in living in alignment with our values and purpose, and strive to
+                be authentic and transparent in all of our actions.</li>
+              <li><b>Ahimsa:</b> We respect the needs and goals of our clients and work to create solutions
+                that are in harmony with their values and the greater good</li>
+              <li><b>Seva:</b> We are dedicated to serving our clients and helping them achieve their goals,
+                with a focus on building long-term partnerships based on trust and mutual benefit</li>
+              <li><b>Swadharma:</b> We recognize the unique strengths and challenges of each of our
+                clients, and work to help them find their own path to success</li>
+              <li><b> Satya:</b> We value honesty and transparency in our work, and strive to be truthful and
+                authentic in all of our interactions</li>
+            </ul>
           </Col>
         </Row>
         <Row>
-        <div className="about-heading text-center ">
-          <h1>Our Team</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates mollitia dignissimos dolore unde ad alias voluptatibus vero eveniet neque dicta, perferendis perspiciatis, adipisci iure porro possimus, officiis omnis quia a.</p>
-        </div>
+          <div className="about-heading text-center ">
+            <h1>Our Team</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates mollitia dignissimos dolore unde ad alias voluptatibus vero eveniet neque dicta, perferendis perspiciatis, adipisci iure porro possimus, officiis omnis quia a.</p>
+          </div>
+        </Row>
+        {/* <Row className="banner font">
+          <Col>
+            <div >
+              <h2 className="banner-content">We specialize in turning your ideas into software. <br />
+                Give us a call, and we’ll work with you to turn your <br />
+                great idea into reality!</h2>
+            </div>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+                <img className="exclametry-mark" src="../exclamation-mark-90.png" alt="this is innovation"></img>
+              </Col>
+              <Col>
+                <div className='email-about-us'>
+                  <img src="/mail.png" width="25px" height="25px" alt="" /> info@mobedo.com
+                </div>
+                <div className='phone-about-us'>
+                  <img src="/phone.png" width="25px" height="25px" alt="" /> 01897465226
+                </div>
+                <Link to="/contact"> <Button variant="warning" className="btn1">GET IN TOUCH</Button>{' '} </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row> */}
+        <Row>
+          <div className="col_side_heading text-center head">
+            <p>Why Mobedo</p>
+          </div>
+          <p className=" text-center">
+            <strong> MOBEDO </strong> Consulting was conceptualized as a response to our conversations with SME and Startup founders during the COVID pandemic. An unplanned, unexpected event that left every organism and organization unsure. As Design-Thinking proponents, empathy is at the core of our existence. We aim to truly understand the unmet needs and pain points of our clients, and work closely with them to develop tailored solutions that are both effective and compassionate.
+
+          </p>
+          <p className=" text-center">
+            Our commitment to making a positive impact is reflected in everything we do. Our name MOBEDO Consulting, signifies a promise to our clients and our collective resolve with striving to <b> DO MORE</b> for our clients, so they can <b> BE MORE</b>. Our commitment to doing more and being more extends beyond just our business. We also strive to make a positive impact in the communities in which we operate, through a variety of sustainability initiatives
+          </p>
+          <p className=" text-center">
+            The essence of MOBEDO – Our principles, beliefs, philosophies of values
+          </p>
         </Row>
       </Container>
-      <Row className="banner font">
-        <Col>
-          <div >
-            <h2 className="banner-content">We specialize in turning your ideas into software. <br />
-              Give us a call, and we’ll work with you to turn your <br />
-              great idea into reality!</h2>
-          </div>
-        </Col>
-        <Col>
-          <Row>
-            <Col>
-              <img className="exclametry-mark" src="../exclamation-mark-90.png" alt="this is innovation"></img>
-            </Col>
-            <Col>
-              <div className='email-about-us'>
-                <img src="/mail.png" width="25px" height="25px" alt="" /> info@mobedo.com
-              </div>
-              <div className='phone-about-us'>
-                <img src="/phone.png" width="25px" height="25px" alt="" /> 01897465226
-              </div>
-              <Link to="/contact"> <Button variant="warning" className="btn1">GET IN TOUCH</Button>{' '} </Link>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Container className="maincontainer font" >
-      <Row>
-      <div className="col_side_heading text-center head">
-          <p>Why Mobedo</p>
-        </div>
-        <p className=" text-center">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quibusdam, amet quasi saepe sapiente rem nemo dolores quos, libero quas corporis repellat sequi eos commodi magnam alias ex. Aliquam, esse.
-        </p>
-        <p className=" text-center">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quibusdam, amet quasi saepe sapiente rem nemo dolores quos, libero quas corporis repellat sequi eos commodi magnam alias ex. Aliquam, esse.
-        </p>
-        </Row>
-        </Container>
-     
+
 
       <Footer />
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,37 +15,43 @@ import Header from "../components/Header";
 export default function index() {
   return (
     <>
-    <Header/>
-    <Navbar/>
-    <Container fluid className="maincontainer font">
-      <Row className="row_content">
-        <Col>
-          <div className="col_side_heading">
-            <p>We continually strive to DO more, so our clients, our people annd society can BE more.</p>
-          </div>
-        </Col>
-        <Col className="col_side_content">
-          <p>Mobedo Consulting are your strategically for driving growth through data-driven, people-powered, and action-accelerated solutions. We have shed the constraints of conventional consulting to unlock your company's potential</p>
-          <Link to="about"><Button className="button" variant="outline-primary">About us</Button>{' '}</Link>
-        </Col>
-      </Row>
-      {/* <Row>
+      <Header />
+      <Navbar />
+      <Container fluid className="maincontainer font">
+        <Row className="row_content">
+          <Col>
+            <div className="col_side_heading">
+              <p>Facilitating Growth. One Conversation at a time.</p>
+            </div>
+          </Col>
+          <Col className="col_side_content">
+            <p>MOBEDO Consulting is a management consulting firm that is designed to transform businesses through a proven conversational consultative approach. Trust us to guide you in understanding your challenges, setting clear goals and creating a roadmap for success through our personalized solutions</p>
+            {/* <Link to="about"><Button className="button" variant="outline-primary">About us</Button>{' '}</Link> */}
+          </Col>
+        </Row>
+        {/* <Row>
         <Col>
           <video className="embed-responsive embed-responsive-16by9 video_content" controls>
             <source className="embed-responsive-item" src={samplevideo} type="video/mp4"></source>
           </video>
         </Col>
       </Row> */}
-      <Row>
-        <div className="col_side_heading ">
-          <p className="head">Our expertise is our service.</p>
-        </div>
-        <div className="col_side_content second-section">
-          <p>
-            We are truth-tellers. We listen, ask the right questions, and elevate strategies to support your transformation.
-          </p>
-        </div>
-        <Col sm className="col_heading_row">
+        <Row>
+          <div className="col_side_heading ">
+            <p className="head">Unleashing Growth.</p>
+          </div>
+          <div className="col_side_content second-section">
+            <p>
+              We identify and build upon the strengths, successes, and positive aspects of your business to create a culture of positivity and innovation, driving sustainable growth and success. Our tailored approach will help you achieve your goals and reach new heights with a strengths-based approach
+            </p>
+          </div>
+          <div className="text-center">
+            <p className="col_side_content">
+              Lets have a conversation<br />
+              Email  Phone Social
+            </p>
+          </div>
+          {/* <Col sm className="col_heading_row">
           <h3>Practices</h3>
           <p className="col_content">What we know.</p>
           <Link to="about"><Button className="learnbutton" variant="outline-primary">Learn More</Button>{' '}</Link>
@@ -59,9 +65,9 @@ export default function index() {
           <h3>Solutuions</h3>
           <p className="col_content">How we deliver.</p>
           <Link to="about"><Button className="learnbutton" variant="outline-primary">Learn More</Button>{' '}</Link>
-        </Col>
-      </Row>
-      {/* <Row>
+        </Col> */}
+        </Row>
+        {/* <Row>
         <div className="col_side_heading">
           <p>What sets us apart.</p>
         </div>
@@ -81,27 +87,27 @@ export default function index() {
         <Col></Col>
       </Row> */}
 
-  
-      <Row className="justify-content-md-center">
-      <div className="col_side_heading head text-center">
-          <p>Our Offerings</p>
-        </div>
-    <Col sm xs lg="2" className="col_heading_row_circle  text-center">
-      <div className="circle ">
-      <img className="bulb" src="../consulting.jpg" alt="this is innovation"></img>
-      </div>
-      <h3 className="consulting" >Consulting</h3>
-    </Col>
-    <Col sm xs lg="2" className="col_heading_row_circle">
-    <div className="circle"></div>
-          <h3  className="startup">Start Up</h3>
-    </Col>
-    <Col sm xs lg="2" className="col_heading_row_circle">
-    <div className="circle"></div>
-          <h3 className="products">Products</h3>
-    </Col>
-  </Row>
-      {/* <Row className="justify-content-md-center">
+
+        {/* <Row className="justify-content-md-center">
+          <div className="col_side_heading head text-center">
+            <p>Our Offerings</p>
+          </div>
+          <Col sm xs lg="2" className="col_heading_row_circle  text-center">
+            <div className="circle ">
+              <img className="bulb" src="../consulting.jpg" alt="this is innovation"></img>
+            </div>
+            <h3 className="consulting" >Consulting</h3>
+          </Col>
+          <Col sm xs lg="2" className="col_heading_row_circle">
+            <div className="circle"></div>
+            <h3 className="startup">Start Up</h3>
+          </Col>
+          <Col sm xs lg="2" className="col_heading_row_circle">
+            <div className="circle"></div>
+            <h3 className="products">Products</h3>
+          </Col>
+        </Row> */}
+        {/* <Row className="justify-content-md-center">
       <div className="col_side_heading  text-center">
           <p>Our Offerings</p>
         </div>
@@ -120,56 +126,56 @@ export default function index() {
         </Col>
        
       </Row> */}
-    
 
-      <Row>
-      <div className="col_side_heading text-center head">
-          <p>Why Mobedo</p>
-        </div>
-        <p className=" text-center">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quibusdam, amet quasi saepe sapiente rem nemo dolores quos, libero quas corporis repellat sequi eos commodi magnam alias ex. Aliquam, esse.
-        </p>
-        <p className=" text-center">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quibusdam, amet quasi saepe sapiente rem nemo dolores quos, libero quas corporis repellat sequi eos commodi magnam alias ex. Aliquam, esse.
-        </p>
-        <Col sm className="col_heading_row  col_heading_row_circle ">
-          <div className="circle   ">
-            <img className="bulb" src="../bulb.png" alt="this is innovation"></img>
+
+        {/* <Row>
+          <div className="col_side_heading text-center head">
+            <p>Why Mobedo</p>
           </div>
-          <h3 className="consulting" >INNOVATION</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda nulla omnis laboriosam doloribus nobis velit ducimus magni labore voluptatum animi totam ipsam perferendis, quaerat saepe maiores rerum possimus necessitatibus quod!</p>
-        </Col>
-        <Col sm className="col_heading_row">
-          <div className="circle">
-          <img className="thumb" src="../thumb.png" alt="this is innovation"></img>
-          </div>
-          <h3 className="consulting" >QUALITY</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
-        </Col>
-        <Col sm className="col_heading_row">
-          <div className="circle">
-          <img className="clock" src="../clock.png" alt="this is innovation"></img>
-          </div>
-          <h3 className="consulting" >EXPERIENCE</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
-        </Col>
-        <Col sm className="col_heading_row">
-          <div className="circle">
-          <img className="people" src="../people.png" alt="this is innovation"></img>
-          </div>
-          <h3 className="consulting" >HAPPY CLIENTS</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
-        </Col>
-        <Col sm className="col_heading_row">
-          <div className="circle">
-          <img className="message" src="../message.png" alt="this is innovation"></img>
-          </div>
-          <h3 className="consulting" >SUPPORT</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
-        </Col>
-      </Row>
-    </Container>
-    <Footer/>
+          <p className=" text-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quibusdam, amet quasi saepe sapiente rem nemo dolores quos, libero quas corporis repellat sequi eos commodi magnam alias ex. Aliquam, esse.
+          </p>
+          <p className=" text-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto quibusdam, amet quasi saepe sapiente rem nemo dolores quos, libero quas corporis repellat sequi eos commodi magnam alias ex. Aliquam, esse.
+          </p>
+          <Col sm className="col_heading_row  col_heading_row_circle ">
+            <div className="circle   ">
+              <img className="bulb" src="../bulb.png" alt="this is innovation"></img>
+            </div>
+            <h3 className="consulting" >INNOVATION</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda nulla omnis laboriosam doloribus nobis velit ducimus magni labore voluptatum animi totam ipsam perferendis, quaerat saepe maiores rerum possimus necessitatibus quod!</p>
+          </Col>
+          <Col sm className="col_heading_row">
+            <div className="circle">
+              <img className="thumb" src="../thumb.png" alt="this is innovation"></img>
+            </div>
+            <h3 className="consulting" >QUALITY</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
+          </Col>
+          <Col sm className="col_heading_row">
+            <div className="circle">
+              <img className="clock" src="../clock.png" alt="this is innovation"></img>
+            </div>
+            <h3 className="consulting" >EXPERIENCE</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
+          </Col>
+          <Col sm className="col_heading_row">
+            <div className="circle">
+              <img className="people" src="../people.png" alt="this is innovation"></img>
+            </div>
+            <h3 className="consulting" >HAPPY CLIENTS</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
+          </Col>
+          <Col sm className="col_heading_row">
+            <div className="circle">
+              <img className="message" src="../message.png" alt="this is innovation"></img>
+            </div>
+            <h3 className="consulting" >SUPPORT</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae fugit voluptates necessitatibus? Blanditiis sint quaerat provident ex vero laborum. Facilis eos esse tempore commodi similique! Quis ea omnis necessitatibus?</p>
+          </Col>
+        </Row> */}
+      </Container>
+      <Footer />
     </>
   )
 
